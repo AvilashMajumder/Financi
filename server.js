3423ce5af45f4f08fd4f6684b0bc22c442f94d0f
AvilashMajumder <avilashmajumder13@gmail.com>
2026-04-03 02:48:24 +0530

    Database connected

--- server.js
+++ server.js
@@ -13,3 +13,16 @@ dotenv.config(
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+connectDB()
+.then(() => {
+    app.on("error", (error) => {
+        console.log("Error: ", error);
+        throw error;
+    })
+    app.listen(process.env.PORT || 5000, () => {
+        console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    })
+})
+.catch((err) => {
+    console.log("MongoDB connection failed, server not started ", err);
+})
