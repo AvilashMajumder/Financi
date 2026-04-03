ecd823bb7a7ba0224551f09b1f29123eec918d75
AvilashMajumder <avilashmajumder13@gmail.com>
2026-04-04 03:24:34 +0530

    Created file structure, Added models

---
+++ models/Transaction.js
@@ -0,0 +1,23 @@
+import mongoose, {Schema} from "mongoose";
+
+const transactionSchema = new mongoose.Schema(
+    {
+        amount: { type: Number, required: [true, 'Amount is required'], min: [0.01, 'Amount must be greater than 0'] },
+        type: { type: String, required: [true, 'Type is required'], enum:{values: ['Income', 'Expense'], message: 'Transaction must be either income or expense' }},
+        category: { type: String, required: [true, 'Category is required'], trim: true},
+        date: { type: Date, required: [true, 'Date is required'], trim: true, default: Date.now },
+        notes: { type: String, trim: true, default: "" },
+        createdBy: {type: mongoose.Schema.Types.ObjectId, ref:"User", required: true},
+        isDeleted: {type: Boolean, default: false}
+    },
+    {timestamps: true}
+);
+
+// Used to filter out deleted transactions in all find queries(find, findOne, findById, findOneAndUpdate, etc.), so that they are not returned in results
+// Internally works as Transaction.find({isDeleted: false}) // return all non deleted transactions
+transactionSchema.pre(/^find/, function(next){
+    this.where({isDeleted: false});
+    next();
+});
+
+export const Transaction = mongoose.model("Transaction", transactionSchema);
---
+++ models/User.js
@@ -0,0 +1,30 @@
+import mongoose, {Schema} from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema(
+    {
+        username: { type: String, required: [true, 'Username is required'], unique: true, lowercase: true, trim: true, index: true },
+        email: { type: String, required: [true, 'Email is required'], unique: true, lowercase: true, trim: true, match: [/^\S+@\S+\.\S+$/, "Please enter a valid email"],},
+        password: { type: String, required: [true, 'Password is required'], minlength: [6, "Password must be at least 6 characters"] },
+        role: {type: String, enum: ['User', 'Analyst', 'Admin']},
+        isActive: {type: Boolean, default: true},
+        refreshToken: {type: String}
+    },
+    {timestamps: true}
+);
+
+// Before saving the user, Hash the password(if Modified) using bcrypt
+userSchema.pre("save", async function(next){
+    if(!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+// Compare passwords to check if correct
+userSchema.methods.isPasswordCorrect = async function(password){
+    if(typeof password !== 'string' || !password) return false;
+    if(!this.password) return false;
+    return bcrypt.compare(password, this.password);
+}
+
+export const User = mongoose.model("User", userSchema);
