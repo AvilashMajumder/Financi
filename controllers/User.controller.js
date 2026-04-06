ccb73d1699fb24cd5340a8b9a303815f0f09695b
AvilashMajumder <avilashmajumder13@gmail.com>
2026-04-06 10:56:09 +0530

    Improve error handling in registerUser by returning specific error messages

--- controllers/User.controller.js
+++ controllers/User.controller.js
@@ -56,7 +56,7 @@ export const registerUser = async (req, res) => {
             .status(201)
             .json({ message: "User created successfully", user: created });
     } catch (err) {
-        return res.status(500).json({ message: "Server error" });
+        return res.status(500).json({ message: err.message });
     }
 };
 
